1b0a054d4d22e8f5ffe5ece58a48a80dc27a35f2
Patrik Fusek <patrik.fusek2@gmail.com>
2021-05-27 12:31:59 +0200

    Two algorithms which can calculate large numbers and also compare both of them

--- src/BigIntCalc.java
+++ src/BigIntCalc.java
@@ -3,14 +3,54 @@ import java.math.BigInteger;
 
 public class BigIntCalc {
 
+    //======================================BitInteger Algorithm====================================
+    //==============================================================================================
+
     public static BigInteger multiplication(String number1, String number2)
     {
-        
         BigInteger largeNum = new BigInteger(number1);
         BigInteger anotherLargeNum = new BigInteger(number2);
         largeNum = largeNum.multiply(anotherLargeNum);
-        System.out.println("The result of multiplication these nums is: " + largeNum);
 
         return largeNum;
     }
+
+    //======================================Manual Multiplication Alogorithm========================
+    //==============================================================================================
+
+    public static String basicMultiplication(int number1[], int number2[]) {
+       
+        int result[] = new int[number1.length + number2.length - 1];
+        for (int i = number2.length - 1; i >= 0; i--) {
+            for (int j = number1.length - 1; j >= 0; j--) {
+                result[i + j] += number2[i] * number1[j];
+            }
+        }
+       
+        for (int k = result.length - 1; k > 0; k--) {
+            int remainingNum = result[k] / 10;
+            result[k - 1] += remainingNum;
+            result[k] = result[k] % 10;
+        }
+
+        StringBuffer resultStr = new StringBuffer();
+        for (int m = 0; m < result.length; m++) {
+            resultStr.append(result[m]);
+        }
+        return resultStr.toString();
+    }
+
+    //======================================Method for comparing these 2 algorithms=================
+    //==============================================================================================
+
+    public static boolean compare(Object al1, Object al2)
+    {
+        if (al1.toString().equals(al2))
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    
 }
--- src/mainCalculation.java
+++ src/mainCalculation.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-import java.math.BigInteger;
 
 public class mainCalculation {
 
     public static void main(String[] args) {
 
+        //Here you input 2 large numbers which are stored in string format
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the number1: ");
         String num1 = sc.nextLine();
@@ -12,12 +12,27 @@ public class mainCalculation {
         System.out.print("Please enter the number2: ");
         String num2 = sc.nextLine();
 
-        System.out.println( "Big Number1: " + num1 );
-        System.out.println("Big Number2: " + num2 );
+        //Strings are transferred into Integer arrays
+        int[] numArr1 = new int[num1.length()];
+        int[] numArr2 = new int[num2.length()];
 
+        for(int i = num1.length()-1; i >= 0; i--)
+        {
+            numArr1[i] = num1.charAt(i) - '0';
+        }
+        for(int j = num2.length()-1; j >= 0; j--)
+        {
+            numArr2[j] = num2.charAt(j) - '0';
+        }
+
+        //Created an object where I implement 2 different algorithms of calculation and provide them
         BigIntCalc result = new BigIntCalc();
-        result.multiplication(num1, num2);
+        System.out.println("Al1: The result of multiplication is: " + result.multiplication(num1, num2));
+        System.out.println("Al2: The result of multiplication is: " + result.basicMultiplication(numArr1, numArr2));
 
+        //Checking if these 2 algorithms have the same result value
+        System.out.println("checking algorithms... ");
+        System.out.println("Comparing algorithms:" + result.compare(result.multiplication(num1, num2), result.basicMultiplication(numArr1, numArr2)));
 
     }
 }
